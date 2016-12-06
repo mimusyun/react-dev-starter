8db01ac3108d36da7630080ca899f46304c794e9
Mimura Shunichiro <mimuramimusyun@gmail.com>
2016-12-06 14:28:09 +0100

    update sample

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,29 +2,51 @@ import React from 'react'
 import Profile from './Profile.jsx'
 import AddProfile from './AddProfile.jsx'
 
+/*Functional Component example*/
+// var MyFunctionalComponent = props => (
+//     <div>
+//         <p>I am a functional Component!!!</p>
+//         <p> {props.x } </p>
+//     </div>
+// )
+
 export default class App extends React.Component {
     
     constructor(props) {
         super(props)
         this.state = {
             profiles : [
-                {
-                    id: 0,
-                    name: 'David', 
-                    age: 30,
-                    biography: 'enjoys swimming and biking',
-                    hobbies: ['swimming', 'biking']
-                },
-                {
-                    id: 1,
-                    name: 'Bill', 
-                    age: 42,
-                    biography: 'enjoys long walks on the beach',
-                    hobbies: ['gardening', 'gaming']
-                }
+                // {
+                //     id: 0,
+                //     name: 'David', 
+                //     age: 30,
+                //     biography: 'enjoys swimming and biking',
+                //     hobbies: ['swimming', 'biking']
+                // },
+                // {
+                //     id: 1,
+                //     name: 'Bill', 
+                //     age: 42,
+                //     biography: 'enjoys long walks on the beach',
+                //     hobbies: ['gardening', 'gaming']
+                // }
             ]
         }
         this.addUser = this.addUser.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8888/profiles')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            this.setState({
+                profiles : data
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
     addUser(newProfile) {
@@ -36,9 +58,8 @@ export default class App extends React.Component {
     render() {
         let profiles = this.state.profiles.map((profile, index) => {
             return (
-            <div>
+            <div key={profile.id}>
                 <Profile
-                key={profile.id}
                 name={profile.name}
                 age={profile.age}
                 biography={profile.biography}
--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -1,26 +1,45 @@
 import React from 'react'
-export default class Profile extends React.Component {
-    render() {
+
+
+let Profile = props => (
+
+    <div>
+        <h3>Name : {props.name}</h3>
+        <p>AGE: {props.age}</p>
+        <p>BIO: {props.biography}</p>
+        <h3>Hobbies</h3>
+        <ul>
+            {props.hobbies.map((hobby, idx) => <li key={idx}>{hobby}</li>)}
+        </ul>
+        <hr/>
+    </div>
+
+)
+
+export default Profile
+
+// export default class Profile extends React.Component {
+//     render() {
         
-        let hobbies = this.props.hobbies.map((hobby, index) => {
-            return (
-                <li key={index}>
-                    {hobby}
-                </li>
-            )
-        })
+//         let hobbies = this.props.hobbies.map((hobby, index) => {
+//             return (
+//                 <li key={index}>
+//                     {hobby}
+//                 </li>
+//             )
+//         })
         
-        return (
-            <div>
-                <h3>Name : {this.props.name}</h3>
-                <p>AGE: {this.props.age}</p>
-                <p>BIO: {this.props.biography}</p>
-                <h3>Hobbies</h3>
-                <ul>
-                    {hobbies}
-                </ul>
-                <hr/>
-            </div>
-        )
-    }
-}
+//         return (
+//             <div>
+//                 <h3>Name : {this.props.name}</h3>
+//                 <p>AGE: {this.props.age}</p>
+//                 <p>BIO: {this.props.biography}</p>
+//                 <h3>Hobbies</h3>
+//                 <ul>
+//                     {hobbies}
+//                 </ul>
+//                 <hr/>
+//             </div>
+//         )
+//     }
+// }
