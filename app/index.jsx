83a7c5c686d66fcc19c88a613631561f5946b026
Mimura Shunichiro <mimuramimusyun@gmail.com>
2016-12-07 16:47:29 +0100

    add index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -2,35 +2,7 @@ import React from 'react'
 import {render} from 'react-dom'
 import ReactDOM from 'react-dom'
 
-var X = React.createClass({
-
-    render: function() {
-        return (
-            <p> XXX </p>
-        )
-    }
-
-})
-
-var Hello = React.createClass({
-    render: function() {
-        return (
-            React.createElement('p', {}, 'Hello World!!!')
-        )
-    }
-})
-
-var App = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <h1>More cool stuff</h1>
-                <p>Render things :)</p>
-                <Hello></Hello>
-                <X></X>
-            </div>
-        )
-    }
-});
+import App from './components/App.jsx'
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
